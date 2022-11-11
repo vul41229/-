c67d4ce924383bf706c75650becba1dc6f1c1951
vul41229 <111473707+vul41229@users.noreply.github.com>
2022-11-11 10:18:14 +0800

    Delete oo directory

--- oo/question/Circle.java
+++
@@ -1,8 +0,0 @@
-package oo.question;
-
-public class Circle extends Question1 {
-    @Override
-    public void draw() {
-        System.out.println("打印一個圓形 O");
-    }
-}
--- oo/question/Square.java
+++
@@ -1,8 +0,0 @@
-package oo.question;
-
-public class Square extends Question1{
-    @Override
-    public void draw() {
-        System.out.println("打印一個方形 口");;
-    }
-}
